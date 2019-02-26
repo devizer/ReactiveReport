39bc897a84d8c1303a12cafade61766c9bf7fb69
devizer <devizer@users.github.com>
2019-02-26 10:57:23 +0200

    Updated

---
+++ compile-only.sh
@@ -0,0 +1,22 @@
+set -e
+work=/ssd/builds/ReactiveReport
+pushd $HOME
+rm -rf $work >/dev/null 2>&1 || true
+cd $(dirname $work)
+git clone https://github.com/devizer/ReactiveReport
+cd ReactiveReport/sources
+
+git pull
+nuget restore
+
+pushd OfflinePrepare
+msbuild /v:n /p:Configuration=Release /t:Rebuild OfflinePrepare.csproj
+popd
+
+pushd WindowsBootAnalyzer
+cd ClientApp; npm install; cd ..
+if [[ $(uname -m) == armv7* ]]; then rid=linux-arm; else rid=linux-arm64; fi; if [[ "$(uname -m)" == "x86_64" ]]; then rid=linux-x64; fi; echo ".NET Core runtime identifier: $rid"
+dotnet publish --self-contained -r $rid -c Release -o bin/public WindowsBootAnalyzer.csproj
+popd
+
+popd
