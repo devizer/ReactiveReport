10f85acb76f3b83a654e7e5e92614f327ac12ea5
devizer <devizer@users.github.com>
2019-04-21 16:58:00 +0300

    Fixing the build

--- sources/publish.cmd
+++ sources/publish.cmd
@@ -1,7 +1,9 @@
+set DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
+setx DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER 0
 pushd WindowsBootAnalyzer\ClientApp
-call npm install
+rem call npm install
 popd
 pushd
-dotnet publish -c Release -o bin\published --self-contained -r win-x64
+dotnet publish -c Release -o bin\published --self-contained -r win-x64 /v:n
 popd
 rem dotnet publish -c Debug bin\Debug
